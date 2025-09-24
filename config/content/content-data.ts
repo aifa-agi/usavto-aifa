b8e3500da904382fe7d3e4aa4f4239f996ca0dab
Next.js starters for AI and more - AIFA <aigenerator.web1v2@gmail.com>
2025-09-24 03:24:01 +0100

    Update menu configuration - 2025-09-24T02:24:01.151Z

--- config/content/content-data.ts
+++ config/content/content-data.ts
@@ -715,7 +715,9 @@ export const contentData = {
         ],
         "keywords": [
           "автоматизация путевых листов"
-        ]
+        ],
+        "isReadyPromptForPerplexity": true,
+        "updatedAt": "2025-09-24T02:18:59.095Z"
       },
       {
         "id": "hpuae76sxx2j79a8tn009ypj",
@@ -1558,5 +1560,5 @@ export const contentData = {
 
 export type contentData = typeof contentData;
 
-export const lastUpdated = "2025-09-24T02:18:59.413Z";
+export const lastUpdated = "2025-09-24T02:24:00.992Z";
 export const generatedBy = "menu-persist-api";
