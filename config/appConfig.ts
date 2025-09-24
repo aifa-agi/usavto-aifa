7fbc43d10c561aa10a13b18165e6d3d31af7f730
Roman Bolshiyanov <romanbolshiyanov@MacBook-Pro-de-Roman.local>
2025-09-24 01:54:51 +0100

    fix update2

--- config/appConfig.ts
+++ config/appConfig.ts
@@ -4,11 +4,22 @@ const site_url = "https://usavto-aifa.vercel.app";
 
 export const appConfig: AppConfig = {
   name: "USAUTO Управление коммерческим автопарком. ",
+  short_name:"USAUTO",
   description:
     "USAUTO Управление коммерческим автопаркомОблачное решение комплексного управления коммерческим автомобильным парком.",
   url: site_url,
   ogImage: `${site_url}/images/usautopro1.jpg`,
   manifest: "/manifest.json",
-
+  lang: "ru",
   mailSupport: "support@aifa.dev",
+  illustrations: {
+    loading: {
+      dark: "/_static/illustrations/idea-launch.svg",
+      light: "/_static/illustrations/success.svg",
+    },
+  },
+
+  logo: "/logo.png",
 };
+
+
