5abc6edfc99360de4e3bccd40bc5101deae7f99f
Next.js starters for AI and more - AIFA <aigenerator.web1v2@gmail.com>
2025-10-14 20:22:03 +0100

    Update menu configuration - 2025-10-14T19:22:03.171Z

--- config/content/content-data.ts
+++ config/content/content-data.ts
@@ -9564,5 +9564,5 @@ export const contentData = {
 
 export type contentData = typeof contentData;
 
-export const lastUpdated = "2025-10-14T19:21:34.072Z";
+export const lastUpdated = "2025-10-14T19:22:02.913Z";
 export const generatedBy = "menu-persist-api";
