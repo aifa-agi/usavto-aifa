ae7c9bc006d0383074da5f9f5a9592b4efc9f3a9
Next.js starters for AI and more - AIFA <aigenerator.web1v2@gmail.com>
2025-10-25 18:59:38 +0100

    Update menu configuration - 2025-10-25T17:59:38.323Z

--- config/content/content-data.ts
+++ config/content/content-data.ts
@@ -1090,7 +1090,7 @@ export const contentData = {
         "updatedAt": "2025-10-25T17:37:16.248Z",
         "order": 1,
         "badgeName": "Блог",
-        "description": "как самозанятому получить лицензию для работы в такси",
+        "description": "Узнайте, как самозанятому получить лицензию для работы в такси: пошаговая инструкция, список документов, требования и советы для быстрого оформления!",
         "images": [
           {
             "id": "hk8s4s2wkosqpryjuyv11yyu",
@@ -1133,5 +1133,5 @@ export const contentData = {
 
 export type contentData = typeof contentData;
 
-export const lastUpdated = "2025-10-25T17:59:32.509Z";
+export const lastUpdated = "2025-10-25T17:59:38.210Z";
 export const generatedBy = "menu-persist-api";
